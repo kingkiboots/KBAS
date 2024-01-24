8aa27f1cf9ea35b93b254bf99ff25af7b30fce88
kingkiboots <tuffhr4@gmail.com>
2024-01-24 23:32:13 +0900

    editting readme.md in tenGame

--- src/puzzleAlgorithm/bruteForce/makeTen/MakeTen.java
+++ src/puzzleAlgorithm/bruteForce/makeTen/MakeTen.java
@@ -1,5 +1,14 @@
 package puzzleAlgorithm.bruteForce.makeTen;
 
 public class MakeTen {
-	
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < 1000000000; i++){
+        	
+        }
+        long endTime = System.currentTimeMillis();
+        long secDiffTime = endTime - startTime;
+        
+        System.out.println("secDiffTime : " + secDiffTime);
+    }
 }
