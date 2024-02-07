cf178c78ee5f871449146567667f712a3b5e393a
kingkiboots <tuffhr4@gmail.com>
2024-02-07 19:24:09 +0900

    bruteforce/maketen 소스 정리

--- src/puzzleAlgorithm/bruteForce/makeTen/MakeTen.java
+++ src/puzzleAlgorithm/bruteForce/makeTen/MakeTen.java
@@ -9,13 +9,14 @@ import java.util.*;
 public class MakeTen {
 
     private final Poland poland = new Poland();
+    private final StringBuilder sb = new StringBuilder();
 
-    double EPS = 1e-9; // 충분히 작은 값
-    char[] OPS = {'+', '-', '*', '/'}; // 4개의 연산자
+    private final char[] OPS = {'+', '-', '*', '/'}; // 4개의 연산자
 
-    // 답을 나타내는 계산기를 저장한 배열
+    // 답을 나타내는 계산식를 저장한 배열
     Set<String> res = new HashSet<>();
 
+    // 만들숫자
     int target;
 
     /**
@@ -25,10 +26,63 @@ public class MakeTen {
      * @param r 2번 자리
      */
     private void swap(char[] arr, int f, int r){
+        if(f == r) return;
         char temp;
         temp = arr[f];
         arr[f] = arr[r];
         arr[r] = temp;
+    }
+    /**
+     * 배열에서 두 개 요소의 자리를 바꿔주는 메소드
+     * @param arr 배열
+     * @param f 1번 자리
+     * @param r 2번 자리
+     */
+    private void swap(int[] arr, int f, int r){
+        if(f == r) return;
+        int temp;
+        temp = arr[f];
+        arr[f] = arr[r];
+        arr[r] = temp;
+    }
+    /**
+     * 4개의 수를 조합하여 24개의 경우의 수를 만들어내는 메소드
+     * @param val 입력받은 4개의 숫자
+     */
+    private Set<String> generatePermutation(int[] val){
+        Set<String> everyCases = new HashSet<>();
+        permute(val, 0, everyCases);
+        return everyCases;
+    }
+
+    /**
+     * 재귀 함수를 이용하여 입력받은 4개의 숫자를 이용해 조합할 수 있는 모든 경우의 수를 만들어낸다.
+     * @param val 입력받은 4개의 숫자
+     * @param start 여기 인덱스의 요소에 i 인덱스의 숫자가 들어가서 만들 수 있는 모든 경우의 수를 계산한다.
+     *              start + 1로 permute 함수를 재귀적으로 호출하여, start 다음 위치부터 나머지 배열에 대한 모든 순열을 생성
+     * @param everyCases 모든 경우의 수가 담길 Set
+     */
+    private void permute(int[] val, int start, Set<String> everyCases){
+        if(start >= val.length){
+            for(int num : val) sb.append(num);
+            everyCases.add(sb.toString());
+            sb.setLength(0); // StringBuilder 초기화
+
+//            System.out.println("******************************");
+//            System.out.println("enlist");
+//            System.out.println("******************************");
+        } else {
+            for (int i = start; i < val.length; i++){
+                swap(val, start, i);
+//                System.out.println("=================================================");
+//                System.out.println("start : " + start);
+//                System.out.println("i : " + i);
+//                System.out.println("{"+val[0]+","+val[1]+","+val[2]+","+val[3]+"}");
+//                System.out.println("=================================================");
+                permute(val, start + 1, everyCases);
+                swap(val, start, i);
+            }
+        }
     }
 
     /**
@@ -38,6 +92,7 @@ public class MakeTen {
     private void check(String exp){
         // 계산 결과와 만들고자 하는 수의 값의 차가 충분히 작을 때 일치한다고 가정한다.
         double calcRes = poland.calcPoland(exp);
+        double EPS = 1e-9; // 충분히 작은 값
         if(Math.abs(calcRes - target) < EPS){
             String decodedPoland = poland.decodePoland(exp);
             res.add(decodedPoland);
@@ -47,24 +102,24 @@ public class MakeTen {
     /**
      * 텐퍼즐 프로그램
      * @param val 4개 수를 저장한 배열
-     * @return 4개의 수를 이용하여 target을 만들 수 있는 계산식의 배열
      */
     private void solve (int[] val){
 
-        // 4개의 수 val 의 정렬을 순서대로 시험한다.
-        // ?? 12개의 경우의 수 구하는건 어디갔음? 어딜 가긴 만들어야지
-//        Collections.sort(val); // val을 사전순으로 정렬 및 최소화 한다.
-        String[] everyCases = getEveryCasesFromNumbers(val).toArray(new String[0]); // 0으로 하면 배열 크기 자동 지정
+        // 4개 숫자 배열의 모든 경우의 수 4*3*2*1 = 24
+        String[] everyCases = generatePermutation(val).toArray(new String[0]); // 0으로 하면 배열 크기 자동 지정
+        // 4개의 수 val 의 정렬을 순서대로 시험한다. 는 제거
+//        Collections.sort(val); // val을 사전순으로 정렬 및 최소화 한다. 는 제거
         int len = everyCases.length;
         int i = 0;
         do {
             // 4개의 문자를 연결해 가능한 문자열 fours를 만든다.
             String fours = everyCases[i];
-//            for(int v : val) fours += String.valueOf(v);
             // 4의 3제곱 = 64가지의 연산자 조합을 알아본다
             for (char op1 : OPS){
                 for (char op2 : OPS){
                     for (char op3 : OPS){
+
+                        // 5가지 패턴을 모두 탐색한다.
                         // 우선 패턴 xxxxooo을 만든다.
                         char[] exp = (fours + op1 + op2 + op3).toCharArray();
 
@@ -93,42 +148,6 @@ public class MakeTen {
         } while (i < len);
     }
 
-    /**
-     * 4개의 수를 조합하여 12개의 경우의 수를 만들어내는 메소드
-     * @param val 입력받은 4개의 숫자
-     * @return 4개의 수를 조합하여 만든 12개의 경우의 수 문자열
-     */
-    private Set<String> getEveryCasesFromNumbers(int[] val){
-        int len = val.length;
-        Set<String> everyCases = new HashSet<>();
-        for(int i = 0; i < len; i++){
-            String str = "";
-            str += String.valueOf(val[i]);
-            for(int j = 0; j < len; j++){
-                if(j != i) {
-                    str += String.valueOf(val[j]);
-                    for (int k = 0; k < len; k++) {
-                        if(k != j && k != i){
-                            str += String.valueOf(val[k]);
-                            for(int l = 0; l < len; l++){
-                                if(l != k && l != j && l != i) {
-                                    str += String.valueOf(val[l]);
-                                    everyCases.add(str);
-                                    str = str.substring(0, 3);
-                                }
-                            }
-                            str = str.substring(0, 2);
-                        }
-                    }
-                    str = str.substring(0, 1);
-                }
-            }
-        }
-        return everyCases;
-    }
-
-
-
     public static void main(String[] args) throws IOException {
         MakeTen makeTen = new MakeTen();
         int[] val = new int[4];
@@ -140,7 +159,6 @@ public class MakeTen {
         }
         System.out.print("target number : ");
         makeTen.target = Integer.parseInt(br.readLine());
-
 
         // 텐퍼즐을 푼다.
         makeTen.solve(val);
