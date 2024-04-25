0b3989229eb71e38ac6f9961152f679367819864
kingkiboots <tuffhr4@gmail.com>
2024-04-26 00:10:19 +0900

    ADD) 240423 : 재귀호출 조합, 순열 응용 문제 작성

---
+++ src/puzzleAlgorithm/basic/recursive/basic/combination/AdvancedCombination.java
@@ -0,0 +1,16 @@
+package puzzleAlgorithm.basic.recursive.basic.combination;
+
+/**
+ * 조합 응용문제
+ * 백설공주와 9난장이가 되었다.
+ *
+ * 원년 멤버인 7명을 추리려고 하는데 7명 합쳐서 키가 100이었던 게 기억난다.
+ *
+ * 다들 키를 측정해보니깐 아래와 같다.
+ *
+ * 20 7 23 19 10 15 25 8 13
+ *
+ * 자, 원년 멤버 7명을 맞추자
+ */
+public class AdvancedCombination {
+}
---
+++ src/puzzleAlgorithm/basic/recursive/basic/permutation/AdvancedPermutation.java
@@ -0,0 +1,13 @@
+package puzzleAlgorithm.basic.recursive.basic.permutation;
+
+/**
+ * 0 ~ 9 비밀번호
+ *
+ * 서로 다른 숫자
+ *
+ * 곱하거나 더해서 220이 되는 4개의 숫자
+ *
+ * 비밀번호가 되는 모든 후보를 구하시오
+ */
+public class AdvancedPermutation {
+}
