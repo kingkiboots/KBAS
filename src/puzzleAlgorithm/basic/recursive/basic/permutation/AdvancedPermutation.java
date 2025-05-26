520bcd28af7d7472da555a041ae8f5bf9108175f
e4_kingkiboots <tuffhr4@gmail.com>
2025-05-26 15:23:24 +0900

    Refactor: 재귀 - 순열 응용문제 리팩토링

--- src/puzzleAlgorithm/basic/recursive/basic/permutation/AdvancedPermutation.java
+++ src/puzzleAlgorithm/basic/recursive/basic/permutation/AdvancedPermutation.java
@@ -1,27 +1,21 @@
 package puzzleAlgorithm.basic.recursive.basic.permutation;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * 0 ~ 9 비밀번호
- *
  * 서로 다른 숫자
- *
  * 곱하거나 더해서 220이 되는 4개의 숫자
- *
  * 비밀번호가 되는 모든 후보를 구하시오
  */
-public class AdvancedPermutation {
-
-    final int[] N = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    final int R = 4;
-    final int target = 220;
+public class AdvancedPermutation extends AbstractPermutation {
+    final int target;
     StringBuilder candidates = new StringBuilder();
 
-    int[] result = new int[R];
-    boolean[] checkList = new boolean[N.length];
+    AdvancedPermutation(int[] N, int R, int target) {
+        super(N, R);
+        this.target = target;
+    }
 
     /**
      * +++
@@ -35,54 +29,58 @@ public class AdvancedPermutation {
      */
     void calculate(){
         if((result[0] + result[1] + result[2] + result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] + result[1] + result[2] * result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] + result[1] * result[2] + result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] + result[1] * result[2] * result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] * result[1] + result[2] + result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] * result[1] + result[2] * result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] * result[1] * result[2] + result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
         if((result[0] * result[1] * result[2] * result[3]) == target){
-            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "") + "\n");
+            candidates.append(Arrays.toString(result).replaceAll("[^0-9]", "")).append("\n");
         }
     }
 
-    void DFS(int depth, int begin){
+    protected void DFS(int depth){
         if(depth == R){
             calculate();
             return;
         }
         for (int i = 0; i < N.length; i++){
             if(checkList[i]){
-               continue;
+                continue;
             }
             result[depth] = N[i];
             checkList[i] = true;
-            DFS(depth + 1, i + 1);
+            DFS(depth + 1);
             checkList[i] = false;
         }
     }
 
     void solve () {
-        DFS(0, 0);
-        System.out.println(candidates);
+        DFS(0);
+        System.out.println(candidates.toString());
     }
 
     public static void main(String[] args) {
-        AdvancedPermutation advancedPermutation = new AdvancedPermutation();
+        int[] N = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int R = 4;
+        int target = 220;
+
+        AdvancedPermutation advancedPermutation = new AdvancedPermutation(N, R, target);
         advancedPermutation.solve();
     }
 }
