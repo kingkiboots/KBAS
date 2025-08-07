612389c08332143205db2bd5fb4301c99842baac
kingkiboots <tuffhr4@gmail.com>
2025-08-07 21:20:50 +0900

    Feat: 하노이탑 문제 풀이 추가

--- src/puzzleAlgorithm/basic/recursive/towerOfHanoi/TowerOfHanoi.java
+++ src/puzzleAlgorithm/basic/recursive/towerOfHanoi/TowerOfHanoi.java
@@ -1,4 +1,37 @@
 package puzzleAlgorithm.basic.recursive.towerOfHanoi;
 
 public class TowerOfHanoi {
+
+    StringBuilder sb = new StringBuilder();
+
+    private void move(int N, String from, String to) {
+        sb.append(N)
+            .append("번 원판을 ")
+            .append(from)
+            .append("에서 ")
+            .append(to)
+            .append("로 옮깁니다.")
+            .append("\n");
+    }
+
+    private void hanoi(int N, String from, String to, String via) {
+        if(N == 1) {
+            move(1, from, to);
+        }
+        else {
+            hanoi(N-1, from, via, to);
+            move(N, from, to);
+            hanoi(N-1, via, to, from);
+        }
+    }
+
+    protected void solve(int N) {
+        hanoi(N, "A", "C", "B");
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        TowerOfHanoi towerOfHanoi = new TowerOfHanoi();
+        towerOfHanoi.solve(3);
+    }
 }
